2e09e3aae496ff2b064e66a8edf864f7b8c6fc3f
Camilo Plaza <cplaza@opentrends.net>
2024-03-15 12:45:32 +0100

    Initial Commit

---
+++ Accessibility_001/Accessibility_001App.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_001App.swift
+//  Accessibility_001
+//
+//  Created by Camilo Plaza on 15/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Accessibility_001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accessibility_001/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Accessibility_001
+//
+//  Created by Camilo Plaza on 15/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
