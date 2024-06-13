55fc3d113f8e6d317fbb817037be66286a1170e7
Camilo Plaza <cplaza@opentrends.net>
2024-06-13 16:24:39 +0200

    BEFORE ACTIVE Swift6

--- Accessibility_001/FontCustomDynamicSizeView.swift
+++ Accessibility_001/FontCustomDynamicSizeView.swift
@@ -43,7 +43,7 @@ struct FontCustomDynamicSizeView: View {
             
             Divider()
             
-            Text("Size: \(UIFont.customFont?.pointSize)\nLineHeight: \(UIFont.customFont?.lineHeight)")
+            Text("Size: \(UIFont.customFont?.pointSize ?? 0)\nLineHeight: \(UIFont.customFont?.lineHeight ?? 0)")
             
             Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
                 .font(.customFont)
